44342bf75954a16732db283df197b6584fc7e9d0
Alexey Landik <prosperous219@gmail.com>
2024-06-18 11:32:51 +0300

    Дипломный проект. Задание 1: Юнит-тесты
    Убрал закомментированный код

--- tests/test_bun.py
+++ tests/test_bun.py
@@ -3,12 +3,8 @@ from ..config import Config
 
 class TestBun:
 
-    # Тест метода get_name. Возвращает имя булки
     def test_get_name_string_name(self, create_bun):
-        # bun = Bun(name=Config.BUN_NAME, price=Config.BUN_PRICE)
         assert create_bun.get_name() == Config.BUN_NAME
 
-    # Тест метода get_price. Возвращает цену булки
     def test_get_price_float_price(self, create_bun):
-        # bun = Bun(name=Config.BUN_NAME, price=Config.BUN_PRICE)
         assert create_bun.get_price() == Config.BUN_PRICE
